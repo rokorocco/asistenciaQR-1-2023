35f06efef1f42c7705f5e7c8a2ad20c414262abf
rokorocco <123479520+rokorocco@users.noreply.github.com>
2023-01-26 13:37:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbztstY4T60Sg5QchtKJDUaJ6H7rFktl07bcEnsspjnT73kOrIGr4z9nF83WeJRb9ivO-g/exec";
